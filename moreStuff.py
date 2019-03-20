6856f4047ae0a1fdbd359de9bec90b1d6474dd74
ccole71 <ccole71@pytrainprdweb.kennesaw.edu>
2019-03-20 16:20:03 -0400

    Initial commit with files from first training session

---
+++ moreStuff.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+c = a + b
+print(c)
+d = "hello"
+e = "world"
+f = d + " " +  e
+print(f)
---
+++ simpleIf.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+var = 99
+if(var == 100):
+	print("Value of expression is 100")
+print("Good Bye!")
